adf8044727d8180852a2a223dc561fcc207b8756
Somi <somy.taheri@gmail.com>
2014-11-26 10:14:31 +1100

    google-map fixed

--- include/js/info.js
+++ include/js/info.js
@@ -1,53 +1,40 @@
 $("#infoIcon").click(
-
 	function toggle() {
 		var winHeight=$( window ).height();
-		//var windowOffset=$(window).height()+$(window).scrollTop()
-		//var windowOffset=$(window).scrollTop()
 		var footerHeight=$( "#footer" ).outerHeight();
-		if ( $("#info").css("display")=="none") {
+		if ( $("#info").css("display")=="none")
+		{
 			$("#info").css("display","block");
-
 			$("#info").css("top",winHeight);
-			//console.log($("#info").css("top"))
 			$('#info').animate({
-				top:footerHeight,
-
-				//
-			}, 2000, function(){
-				//$("#info").css("margin-top","150px");
+				top:footerHeight
+			}, 2000, function()
+			{
 				$("#album").css("display","none");
-				console.log($("#info").css("top"));
 			});
 
-			$('#footer').animate({
+			$('#footer').animate(
+			{
 				bottom: winHeight - footerHeight,
-				backgroundColor: 'rgba(47, 52,59,1)',
-			}, 2000, function(){
-console.log($("#footer").css("bottom"));
+				backgroundColor: 'rgba(47, 52,59,1)'
+			}, 2000, function()
+			{
 				console.log($("#info").css("top"));
 				console.log($("#footer").css("background-color"));
 				$("#footer").css("position","absolute");
 				$("#info").css("position","absolute");
 				$( ".fa-info" ).removeClass( "fa-info" ).addClass( "fa-times" );
-
-
 			});
-			$('.showIcon').animate({
-				opacity:"0",
-			}, 2000, function(){
-			});
-
-
-
-
-
-
+			$('.showIcon').animate(
+				{
+				opacity:"0"
+			}, 2000
+			);
 			return false;
 		}
-		else {
+		else
+		{
 			$("#album").css("display","block");
-			// $("#info").css("top",winHeight);
 			$('#info').animate({
 				top:winHeight
 			}, 2000, function(){
@@ -55,22 +42,23 @@ console.log($("#footer").css("bottom"));
 			});
 			$('#footer').animate({
 				bottom: 0,
-				backgroundColor: 'rgba(47, 52,59,0.9)',
+				backgroundColor: 'rgba(47, 52,59,0.9)'
 			}, 2000, function(){
 				$("#footer").css("position","fixed");
 				$("#info").css("position","fixed");
 				$( ".fa-times" ).removeClass( "fa-times" ).addClass( "fa-info" );
 			});
 			$('.showIcon').animate({
-				opacity:"1",
-			}, 2000, function(){
-			});
-
+				opacity:"1"
+			}, 2000
+			);
 		}
 	});
 
 $("#infoIcon").click( function () {
-	google.maps.event.trigger(map, 'resize');
+	var center = map.getCenter();
+	google.maps.event.trigger(map, "resize");
+	map.setCenter(center);
 
 });
 
